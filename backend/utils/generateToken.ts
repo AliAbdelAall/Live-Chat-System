f5d06ebb5e565f64177741d0d62a5380804f05d8
AliAbdelAall <3laych@gmail.com>
2024-06-06 21:28:15 +0300

    backend: added generateTokenAndSetCookie function

---
+++ backend/utils/generateToken.ts
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import { ObjectId } from "mongoose";
+import { Response } from "express";
+
+export const generateTokenAndSetCookie = (userId: ObjectId, res: Response) => {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET!, {
+		expiresIn: "15d",
+	});
+	res.cookie("jwt", token, {
+		maxAge: 15 * 24 * 60 * 60 * 1000,
+		httpOnly: true,
+		sameSite: "strict",
+	});
+};

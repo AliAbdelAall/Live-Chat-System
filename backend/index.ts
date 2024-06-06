e7655c2a0030f1921306fe2e1c627c0fe7a2f3d5
AliAbdelAall <3laych@gmail.com>
2024-06-06 21:47:27 +0300

    backend: added db connection to app

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,16 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import rootRoutes from "./routes/root.routes";
+import { connect } from "./config/db.config";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app: Express = express();
 
+app.use(express.json());
+app.use("/api", rootRoutes);
+
 app.listen(port, () => {
 	console.log(`app is running on port: ${port}`);
+	connect();
 });

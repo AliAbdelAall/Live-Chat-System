5261ed4a5aa8a6714ab129721ecbed7bfa08f2b3
AliAbdelAall <3laych@gmail.com>
2024-06-17 12:23:03 +0300

    backend: added cookieParser

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import rootRoutes from "./routes/root.routes";
 import { connect } from "./config/db.config";
 
@@ -8,6 +9,8 @@ const port = process.env.PORT || 3000;
 const app: Express = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api", rootRoutes);
 
 app.listen(port, () => {

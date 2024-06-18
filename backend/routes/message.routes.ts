c6808aa271df5cac8d5ef035c6e0eeba842537be
AliAbdelAall <3laych@gmail.com>
2024-06-18 14:20:34 +0300

    backend: added getMessages API route

--- backend/routes/message.routes.ts
+++ backend/routes/message.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { sendMessage } from "../controllers/message.controller";
+import { getMessages, sendMessage } from "../controllers/message.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 const messageRoutes = Router();
 
+messageRoutes.get("/:id", authMiddleware, getMessages);
 messageRoutes.post("/send/:id", authMiddleware, sendMessage);
 
 export default messageRoutes;

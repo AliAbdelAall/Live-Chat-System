a1edba7741ef423bd6889b9c4cbcb56538f68198
AliAbdelAall <3laych@gmail.com>
2024-06-18 14:48:35 +0300

    backend: added userRoutes in rootRoutes

--- backend/routes/root.routes.ts
+++ backend/routes/root.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes";
 import messageRoutes from "./message.routes";
+import userRoutes from "./user.routes";
 
 const rootRoutes = Router();
 
 rootRoutes.use("/auth", authRoutes);
 rootRoutes.use("/message", messageRoutes);
+rootRoutes.use("/users", userRoutes);
 
 export default rootRoutes;

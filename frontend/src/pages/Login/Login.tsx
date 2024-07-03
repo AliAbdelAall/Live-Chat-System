c4aaeef79009c8d3a8594eb0c2a6e07c0770fd13
AliAbdelAall <3laych@gmail.com>
2024-07-03 12:42:09 +0300

    frontend: styled Login component

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -2,30 +2,47 @@ import { FC } from "react";
 
 const Login: FC = () => {
 	return (
-		<div>
-			<div>
-				<h1>
+		<div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+			<div className="w-full p-6 flex flex-col rounded-xl shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+				<h1 className="text-3xl font-semibold text-center text-gray-300">
 					Login
-					<span> Chat App</span>
+					<span className="text-blue-400"> Chat App</span>
 				</h1>
 				<div>
 					<div>
-						<label>
-							<span>Username</span>
+						<label className="label">
+							<span className="text-base label-text">
+								Username
+							</span>
 						</label>
-						<input type="text" placeholder="Enter username" />
+						<input
+							type="text"
+							placeholder="Enter username"
+							className="w-full input input-bordered h-10"
+						/>
 					</div>
 
 					<div>
-						<label>
-							<span>Password</span>
+						<label className="label">
+							<span className="text-base label-text">
+								Password
+							</span>
 						</label>
-						<input type="text" placeholder="Enter password" />
+						<input
+							type="text"
+							placeholder="Enter password"
+							className="w-full input input-bordered h-10"
+						/>
 					</div>
 
-					<a href="#">Don't have an account?</a>
+					<a
+						href="#"
+						className="text-sm hover:underline hover:text-blue-400 mt-2 inline-block"
+					>
+						Don't have an account?
+					</a>
 
-					<button>Login</button>
+					<button className="btn btn-block mt-2">Login</button>
 				</div>
 			</div>
 		</div>

7fdc7d228cfde7ed3f34f62631121fe9251bc19f
AliAbdelAall <3laych@gmail.com>
2024-07-03 11:09:46 +0300

    frontend: added Login component in App Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import "./index.css";
+import Login from "./pages/Login/Login";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className="p-4 h-screen flex items-center justify-center">
+			<Login />
+		</div>
+	);
 };
 
 export default App;

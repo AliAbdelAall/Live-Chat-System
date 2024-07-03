fb91f2c7cfbe83897c6e0b0ac1894065ddc90b94
AliAbdelAall <3laych@gmail.com>
2024-07-03 12:41:26 +0300

    frontend: added Login component items

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { FC } from "react";
 
-const Login = () => {
-	return <div>Login</div>;
+const Login: FC = () => {
+	return (
+		<div>
+			<div>
+				<h1>
+					Login
+					<span> Chat App</span>
+				</h1>
+				<div>
+					<div>
+						<label>
+							<span>Username</span>
+						</label>
+						<input type="text" placeholder="Enter username" />
+					</div>
+
+					<div>
+						<label>
+							<span>Password</span>
+						</label>
+						<input type="text" placeholder="Enter password" />
+					</div>
+
+					<a href="#">Don't have an account?</a>
+
+					<button>Login</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
